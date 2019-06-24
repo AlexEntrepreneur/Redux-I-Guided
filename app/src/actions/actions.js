3be5d1de73f6063424e1885f3c1fef8ca765e296
AlexEntrepreneur <hello@alexentrepreneur.com>
2019-06-24 09:32:11 +0100

    Action Creators

--- app/src/actions/actions.js
+++ app/src/actions/actions.js
@@ -1,4 +1,17 @@
 // Step-2: Define our Action Types
-const SLIP_ON_BANANA = 'SLIP_ON_BANANA';
-const WATCH_THE_NEWS = 'WATCH_THE_NEWS';
-const WIN_THE_LOTTERY = 'WIN_THE_LOTTERY';
+export const SLIP_ON_BANANA = 'SLIP_ON_BANANA';
+export const WATCH_THE_NEWS = 'WATCH_THE_NEWS';
+export const WIN_THE_LOTTERY = 'WIN_THE_LOTTERY';
+
+// Step-3: Define our Action Creators
+export function slipOnBanana() {
+    return { type: SLIP_ON_BANANA };
+}
+
+export function watchNews() {
+    return { type: WATCH_THE_NEWS };
+}
+
+export function winLottery() {
+    return { type: WIN_THE_LOTTERY };
+}
